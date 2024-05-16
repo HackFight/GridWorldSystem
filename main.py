393995ae9ddf51659e4b3be0042dac99413156b9
HackFight <hackfightowo@gmail.com>
2024-05-16 20:05:20 +0100

    Renderer with Pygame done.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import time
+import renderer
+import pygame
 import grid_system
 import algorythms
 
@@ -12,19 +15,43 @@ def WriteGrid(grid):
         print("")
 
 def main():
+
+    # Initialisation de Pygame
+    pygame.init()
+
+    # Définition des dimensions de la fenêtre
+    SCREEN_WIDTH = 600
+    SCREEN_HEIGHT = 600
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Conway's Game of Life")
     
     grid = grid_system.GridInit(GRID_WIDTH,GRID_HEIGHT)
     grid[1][1] = 1
     grid[1][2] = 1
     grid[1][3] = 1
 
-    WriteGrid(grid)
-    print("")
+    running = True
+    while running:
 
-    grid = algorythms.Conway(grid, GRID_WIDTH, GRID_HEIGHT)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        grid = algorythms.Conway(grid, GRID_WIDTH, GRID_HEIGHT)
 
-    WriteGrid(grid)
-    print("")
+        # Efface l'écran
+        renderer.ClearScreen(screen)
+
+        # Dessine la grille
+        renderer.DrawGrid(grid, GRID_WIDTH, GRID_HEIGHT, screen, SCREEN_WIDTH, SCREEN_HEIGHT)
+    
+        # Mise à jour de l'affichage
+        pygame.display.flip()
+
+        time.sleep(0.1)
+
+# Quitter Pygame
+pygame.quit()
 
 if __name__ == '__main__':
     main()
---
+++ renderer.py
@@ -0,0 +1,18 @@
+import pygame
+
+# Couleurs
+white = (252, 251, 244)
+black = (20, 25, 24)
+
+# Fonction pour dessiner la grille
+def DrawGrid(grid, grid_width, grid_height, screen, screen_width, screen_height):
+    for y in range(grid_height):
+        for x in range(grid_width):
+            if grid[x][y] == 1:
+
+                square_width = screen_width/grid_width
+                square_height = screen_height/grid_height
+                pygame.draw.rect(screen, white, (x * square_width, y * square_height, square_width, square_height))
+
+def ClearScreen(screen):
+    screen.fill(black)
